c7cdea448c689ecfdc08b1a91ba4d58a120e3a2c
UzzielAp <uzzielapariciok01@gmail.com>
2024-12-06 23:50:06 -0500

    Primer commit de mi proyecto

---
+++ ParcialN°1AparicioUzziel_ZhangRichard/Form1.Designer.vb
@@ -0,0 +1,201 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        LABEL = New Label()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        TextNombre = New TextBox()
+        rbtnHombre = New RadioButton()
+        rbtnMujer = New RadioButton()
+        MonthCalendar1 = New MonthCalendar()
+        Button1 = New Button()
+        lblResultado = New Label()
+        btnExit = New Button()
+        ErrorProvider1 = New ErrorProvider(components)
+        ErrorProvider2 = New ErrorProvider(components)
+        CType(ErrorProvider1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(ErrorProvider2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' LABEL
+        ' 
+        LABEL.AutoSize = True
+        LABEL.BackColor = Color.FromArgb(CByte(192), CByte(0), CByte(0))
+        LABEL.BorderStyle = BorderStyle.Fixed3D
+        LABEL.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        LABEL.ForeColor = SystemColors.ButtonFace
+        LABEL.Location = New Point(211, 35)
+        LABEL.Name = "LABEL"
+        LABEL.Size = New Size(217, 32)
+        LABEL.TabIndex = 0
+        LABEL.Text = "TU FUTURO ES HOY "
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(64, 88)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(188, 17)
+        Label1.TabIndex = 1
+        Label1.Text = "Ingrese su nombre y apellido"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(114, 148)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(103, 17)
+        Label2.TabIndex = 2
+        Label2.Text = "Ingrese su sexo"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(64, 198)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(204, 17)
+        Label3.TabIndex = 3
+        Label3.Text = "Ingrese su fecha de nacimiento "
+        ' 
+        ' TextNombre
+        ' 
+        TextNombre.Location = New Point(104, 108)
+        TextNombre.Name = "TextNombre"
+        TextNombre.Size = New Size(123, 23)
+        TextNombre.TabIndex = 4
+        ' 
+        ' rbtnHombre
+        ' 
+        rbtnHombre.AutoSize = True
+        rbtnHombre.Location = New Point(82, 168)
+        rbtnHombre.Name = "rbtnHombre"
+        rbtnHombre.Size = New Size(80, 19)
+        rbtnHombre.TabIndex = 5
+        rbtnHombre.TabStop = True
+        rbtnHombre.Text = "Masculino"
+        rbtnHombre.UseVisualStyleBackColor = True
+        ' 
+        ' rbtnMujer
+        ' 
+        rbtnMujer.AutoSize = True
+        rbtnMujer.Location = New Point(168, 168)
+        rbtnMujer.Name = "rbtnMujer"
+        rbtnMujer.Size = New Size(78, 19)
+        rbtnMujer.TabIndex = 6
+        rbtnMujer.TabStop = True
+        rbtnMujer.Text = "Femenino"
+        rbtnMujer.UseVisualStyleBackColor = True
+        ' 
+        ' MonthCalendar1
+        ' 
+        MonthCalendar1.Location = New Point(45, 226)
+        MonthCalendar1.Name = "MonthCalendar1"
+        MonthCalendar1.TabIndex = 7
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(130, 400)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(87, 31)
+        Button1.TabIndex = 8
+        Button1.Text = "Predicción"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' lblResultado
+        ' 
+        lblResultado.AutoSize = True
+        lblResultado.BorderStyle = BorderStyle.Fixed3D
+        lblResultado.Font = New Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblResultado.ForeColor = SystemColors.ControlText
+        lblResultado.Location = New Point(346, 168)
+        lblResultado.Name = "lblResultado"
+        lblResultado.Size = New Size(93, 19)
+        lblResultado.TabIndex = 9
+        lblResultado.Text = "Su predicción"
+        ' 
+        ' btnExit
+        ' 
+        btnExit.BackColor = Color.Red
+        btnExit.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnExit.ForeColor = SystemColors.Control
+        btnExit.Location = New Point(12, 413)
+        btnExit.Name = "btnExit"
+        btnExit.Size = New Size(61, 27)
+        btnExit.TabIndex = 10
+        btnExit.Text = "X"
+        btnExit.UseVisualStyleBackColor = False
+        ' 
+        ' ErrorProvider1
+        ' 
+        ErrorProvider1.ContainerControl = Me
+        ' 
+        ' ErrorProvider2
+        ' 
+        ErrorProvider2.ContainerControl = Me
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Khaki
+        ClientSize = New Size(648, 452)
+        Controls.Add(btnExit)
+        Controls.Add(lblResultado)
+        Controls.Add(Button1)
+        Controls.Add(MonthCalendar1)
+        Controls.Add(rbtnMujer)
+        Controls.Add(rbtnHombre)
+        Controls.Add(TextNombre)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(LABEL)
+        Name = "Form1"
+        Text = "Form1"
+        CType(ErrorProvider1, ComponentModel.ISupportInitialize).EndInit()
+        CType(ErrorProvider2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents LABEL As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextNombre As TextBox
+    Friend WithEvents rbtnHombre As RadioButton
+    Friend WithEvents rbtnMujer As RadioButton
+    Friend WithEvents MonthCalendar1 As MonthCalendar
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lblResultado As Label
+    Friend WithEvents btnExit As Button
+    Friend WithEvents ErrorProvider1 As ErrorProvider
+    Friend WithEvents ErrorProvider2 As ErrorProvider
+
+End Class
---
+++ ParcialN°1AparicioUzziel_ZhangRichard/Form1.vb
@@ -0,0 +1,186 @@
+﻿'Richard Zhang 8-988-1571'
+'Uzziel Aparicio 8-978-1512'
+Public Class Form1
+    Dim nombreCliente As String
+    Dim sexoCliente As String
+    Dim fechaNacimiento As Date
+    Dim signoZodiacal As String
+    Dim prediccion As String
+    Dim edad As Integer
+    Dim numerosSuerte(2) As String
+
+    Private Sub btnGenerar_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ErrorProvider1.Clear()
+
+        If Not ValidarEntradas() Then
+            Return
+        End If
+
+        Try
+            nombreCliente = TextNombre.Text
+            sexoCliente = If(rbtnHombre.Checked, "Masculino", "Femenino")
+            fechaNacimiento = MonthCalendar1.SelectionStart
+
+            edad = DateTime.Now.Year - fechaNacimiento.Year
+            If DateTime.Now.DayOfYear < fechaNacimiento.DayOfYear Then
+                edad -= 1
+            End If
+
+            signoZodiacal = DeterminarSignoZodiacal(fechaNacimiento)
+            prediccion = ObtenerPrediccion(signoZodiacal)
+
+            GenerarNumerosDeLaSuerte()
+
+            lblResultado.Text = $"Nombre: {nombreCliente}" & vbCrLf &
+                                $"Edad: {edad}" & vbCrLf &
+                                $"Sexo: {sexoCliente}" & vbCrLf &
+                                $"Signo Zodiacal: {signoZodiacal}" & vbCrLf &
+                                $"Predicción: {prediccion}" & vbCrLf &
+                                $"Números de la Suerte: {String.Join(", ", numerosSuerte)}"
+        Catch ex As Exception
+            MessageBox.Show("Ha ocurrido un error: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Function ValidarEntradas() As Boolean
+        Dim esValido As Boolean = True
+
+        If String.IsNullOrEmpty(TextNombre.Text) Then
+            ErrorProvider1.SetError(TextNombre, "Por favor, ingrese su nombre.")
+            esValido = False
+        End If
+
+        If Not (rbtnHombre.Checked OrElse rbtnMujer.Checked) Then
+            ErrorProvider1.SetError(rbtnHombre, "Por favor, seleccione su sexo.")
+            esValido = False
+        End If
+
+        fechaNacimiento = MonthCalendar1.SelectionStart
+        If fechaNacimiento.Year < 1900 OrElse fechaNacimiento.Year > 2024 Then
+            ErrorProvider1.SetError(MonthCalendar1, "El año debe estar entre 1900 y 2024.")
+            esValido = False
+        End If
+
+        Return esValido
+    End Function
+
+    Private Function DeterminarSignoZodiacal(fecha As Date) As String
+        Select Case fecha.Month
+            Case 1
+                If fecha.Day <= 19 Then
+                    Return "Capricornio"
+                Else
+                    Return "Acuario"
+                End If
+            Case 2
+                If fecha.Day <= 19 Then
+                    Return "Acuario"
+                Else
+                    Return "Piscis"
+                End If
+            Case 3
+                If fecha.Day <= 20 Then
+                    Return "Piscis"
+                Else
+                    Return "Aries"
+                End If
+            Case 4
+                If fecha.Day <= 20 Then
+                    Return "Aries"
+                Else
+                    Return "Tauro"
+                End If
+            Case 5
+                If fecha.Day <= 20 Then
+                    Return "Tauro"
+                Else
+                    Return "Géminis"
+                End If
+            Case 6
+                If fecha.Day <= 21 Then
+                    Return "Géminis"
+                Else
+                    Return "Cáncer"
+                End If
+            Case 7
+                If fecha.Day <= 22 Then
+                    Return "Cáncer"
+                Else
+                    Return "Leo"
+                End If
+            Case 8
+                If fecha.Day <= 23 Then
+                    Return "Leo"
+                Else
+                    Return "Virgo"
+                End If
+            Case 9
+                If fecha.Day <= 23 Then
+                    Return "Virgo"
+                Else
+                    Return "Libra"
+                End If
+            Case 10
+                If fecha.Day <= 22 Then
+                    Return "Libra"
+                Else
+                    Return "Escorpio"
+                End If
+            Case 11
+                If fecha.Day <= 22 Then
+                    Return "Escorpio"
+                Else
+                    Return "Sagitario"
+                End If
+            Case 12
+                If fecha.Day <= 21 Then
+                    Return "Sagitario"
+                Else
+                    Return "Capricornio"
+                End If
+        End Select
+        Return ""
+    End Function
+
+    Private Function ObtenerPrediccion(signo As String) As String
+        Select Case signo
+            Case "Aries"
+                Return "Buenas notas"
+            Case "Tauro"
+                Return "Trabajo"
+            Case "Géminis"
+                Return "Rumbas"
+            Case "Cáncer"
+                Return "Dinero"
+            Case "Leo"
+                Return "Deudas"
+            Case "Virgo"
+                Return "Estabilidad laboral"
+            Case "Libra"
+                Return "Felicidad"
+            Case "Escorpio"
+                Return "Amor"
+            Case "Sagitario"
+                Return "Auto nuevo"
+            Case "Capricornio"
+                Return "Paz espiritual"
+            Case "Acuario"
+                Return "Salud"
+            Case "Piscis"
+                Return "Buena suerte en la lotería"
+        End Select
+        Return ""
+    End Function
+
+    Private Sub GenerarNumerosDeLaSuerte()
+        Dim rand As New Random()
+        For i As Integer = 0 To 2
+            numerosSuerte(i) = rand.Next(0, 100).ToString("D2")
+        Next
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+
+End Class
